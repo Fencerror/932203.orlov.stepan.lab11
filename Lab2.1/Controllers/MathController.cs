cab136227f60c873bf44b0c9265b696d8fed0dbb
Fencerror <tyghrtzwrcv@gmail.com>
2024-11-24 23:48:31 +0700

    First commit

---
+++ Lab2.1/Controllers/MathController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MathServiceApp.Models;
+using System;
+
+namespace MathServiceApp.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult UsingModel()
+        {
+            var model = new RandomNumbersViewModel
+            {
+                Num1 = new Random().Next(1, 11),
+                Num2 = new Random().Next(1, 11)
+            };
+
+            return View(model);
+        }
+
+        public IActionResult UsingViewData()
+        {
+            Random random = new Random();
+            ViewData["Num1"] = random.Next(1, 11);
+            ViewData["Num2"] = random.Next(1, 11);
+            return View();
+        }
+
+        public IActionResult UsingViewBag()
+        {
+            Random random = new Random();
+            ViewBag.Num1 = random.Next(1, 11);
+            ViewBag.Num2 = random.Next(1, 11);
+            return View();
+        }
+
+        public IActionResult UsingService()
+        {
+            var model = new RandomNumbersViewModel
+            {
+                Num1 = new Random().Next(1, 11),
+                Num2 = new Random().Next(1, 11)
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Lab2.1/Models/RandomNumbersViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MathServiceApp.Models
+{
+    public class RandomNumbersViewModel
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
